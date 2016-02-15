f7225dd481ef49d521bbb0c30328824df7907d29
marcorosas1991 <marco.rosas@outlook.com>
2016-02-15 16:43:35 -0700

    Create assignment

--- app/src/main/java/com/example/marcorosas/multi_threaded_programming/MainActivity.java
+++ app/src/main/java/com/example/marcorosas/multi_threaded_programming/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.marcorosas.multi_threaded_programming;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -48,5 +62,77 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void createFile(View v) {
+
+        String filename = "numbers.txt";
+        String content = "";
+        FileOutputStream outputStream;
+
+        try {
+            outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
+            outputStream.write(content.getBytes());
+            outputStream.close();
+
+            Toast.makeText(this, "Saved " + getFilesDir(), Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            for (int i = 1; i <= 10; i++) {
+                outputStream = openFileOutput(filename,  MODE_APPEND);
+                outputStream.write((i + "\n").getBytes());
+                outputStream.close();
+
+                Thread.sleep(250);
+            }
+        }
+        catch (Exception error) {}
+    } // end of createFile
+
+    public void loadFile(View v) {
+
+        List<String> numberList = new ArrayList<>();
+
+        try {
+            // open the file for reading
+
+            FileInputStream file = openFileInput("numbers.txt");
+            BufferedReader settingsReader = new BufferedReader(new InputStreamReader(file));
+
+            String line;
+            while ((line = settingsReader.readLine()) != null) {
+                numberList.add(line);
+                Thread.sleep(250);
+            }
+            settingsReader.close();
+            file.close();
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, numberList);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(itemsAdapter);
+    }
+
+} // end of class
+
+class WriteNumbers implements Runnable {
+
+    String filename;
+    FileOutputStream outputStream;
+
+    public WriteNumbers(String filename) {
+        this.filename = filename;
+        run();
+    }
+
+    public void run() {
+
     }
 }
