0ee88a40d664938ac4ea49e7f0687875c8e92133
marcorosas1991 <marco.rosas@outlook.com>
2016-02-15 23:47:59 -0700

    Finish homework

--- app/src/main/java/com/example/marcorosas/multi_threaded_programming/MainActivity.java
+++ app/src/main/java/com/example/marcorosas/multi_threaded_programming/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.marcorosas.multi_threaded_programming;
 
 import android.content.Context;
+import android.content.ContextWrapper;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -12,7 +14,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
+import android.widget.ProgressBar;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -66,73 +68,111 @@ public class MainActivity extends AppCompatActivity {
 
     public void createFile(View v) {
 
-        String filename = "numbers.txt";
-        String content = "";
-        FileOutputStream outputStream;
+        final ProgressBar bar = (ProgressBar) findViewById(R.id.progressBar);
+        bar.setProgress(0);
 
-        try {
-            outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
-            outputStream.write(content.getBytes());
-            outputStream.close();
+        final Handler progressBarHandler = new Handler();
 
-            Toast.makeText(this, "Saved " + getFilesDir(), Toast.LENGTH_SHORT).show();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        new Thread(new Runnable() {
+            public void run() {
+                String filename = "numbers.txt";
+                String content = "";
+                FileOutputStream outputStream;
 
-        try {
-            for (int i = 1; i <= 10; i++) {
-                outputStream = openFileOutput(filename,  MODE_APPEND);
-                outputStream.write((i + "\n").getBytes());
-                outputStream.close();
+                try {
+                    outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
+                    outputStream.write(content.getBytes());
+                    outputStream.close();
 
-                Thread.sleep(250);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                try {
+                    for (int i = 1; i <= 10; i++) {
+                        outputStream = openFileOutput(filename,  MODE_APPEND);
+                        outputStream.write((i + "\n").getBytes());
+                        outputStream.close();
+
+                        progressBarHandler.post(new Runnable() {
+                            public void run() {
+                                bar.setProgress(bar.getProgress() + 10);
+                            }
+                        });
+
+                        Thread.sleep(250);
+                    }
+                }
+                catch (Exception error) {}
             }
-        }
-        catch (Exception error) {}
+        }).start();
+
     } // end of createFile
 
     public void loadFile(View v) {
 
-        List<String> numberList = new ArrayList<>();
+        final ProgressBar bar = (ProgressBar) findViewById(R.id.progressBar);
+        bar.setProgress(0);
 
-        try {
-            // open the file for reading
+        final List<String> numberList = new ArrayList<>();
 
-            FileInputStream file = openFileInput("numbers.txt");
-            BufferedReader settingsReader = new BufferedReader(new InputStreamReader(file));
+        final Handler progressBarHandler = new Handler();
+        final Handler listViewHandler = new Handler();
 
-            String line;
-            while ((line = settingsReader.readLine()) != null) {
-                numberList.add(line);
-                Thread.sleep(250);
+        final ArrayAdapter<String> itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, numberList);
+        final ListView listView = (ListView) findViewById(R.id.listView);
+
+        Thread aThread = new Thread(new Runnable() {
+
+            public void run() {
+                try {
+                    // open the file for reading
+
+                    FileInputStream file = openFileInput("numbers.txt");
+                    BufferedReader settingsReader = new BufferedReader(new InputStreamReader(file));
+
+                    String line;
+                    while ((line = settingsReader.readLine()) != null) {
+                        // do something with the bar
+                        numberList.add(line);
+
+                        progressBarHandler.post(new Runnable() {
+                            public void run() {
+                                bar.setProgress(bar.getProgress() + 10);
+                            }
+                        });
+
+                        Thread.sleep(250);
+                    }
+
+                    settingsReader.close();
+                    file.close();
+
+                    listViewHandler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            listView.setAdapter(itemsAdapter);
+                        }
+                    });
+
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
             }
-            settingsReader.close();
-            file.close();
+        });
 
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
+        aThread.start();
 
-        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, numberList);
+    }
 
+    public void clearList(View v) {
+        ProgressBar bar = (ProgressBar) findViewById(R.id.progressBar);
+        bar.setProgress(0);
+
+        List<String> emptyList = new ArrayList<>();
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, emptyList);
         ListView listView = (ListView) findViewById(R.id.listView);
         listView.setAdapter(itemsAdapter);
     }
 
 } // end of class
-
-class WriteNumbers implements Runnable {
-
-    String filename;
-    FileOutputStream outputStream;
-
-    public WriteNumbers(String filename) {
-        this.filename = filename;
-        run();
-    }
-
-    public void run() {
-
-    }
-}
